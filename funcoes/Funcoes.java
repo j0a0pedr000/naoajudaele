009a46aee82a03e3c744ebb78a7c8860a5a08bd3
j0a0pedr000 <joaopedrofialho790@gmail.com>
2024-09-06 18:33:58 -0300

    Add files via upload

---
+++ funcoes/Funcoes.java
@@ -0,0 +1,18 @@
+package funcoes;
+
+public class Funcoes {
+
+    public static void main(String[] args){
+        soma();
+    }
+
+    public static void soma() {
+        int x = 10;
+        int y = 10;
+        int res;
+        res = x + y;
+
+        System.out.println("resultado: " + res);
+    }
+    
+}
